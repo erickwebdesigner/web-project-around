5dfa6d618289185a25d795d01892e59120760961
erickwebdesigner <erickbarber10@gmail.com>
2024-02-10 10:15:12 -0300

    refactor:the name exchange system via profile javascript has been finalized

--- script/index.js
+++ script/index.js
@@ -2,41 +2,54 @@ const profbtn = document.querySelector('.profile__btn')
 const formEdit = document.querySelector('.form')
 const formclose = document.querySelector('.form__close')
 
-const formElement = document.querySelector('.form__submit')
-
+// Vamos encontrar o formulário no DOM
+const formElement = document.querySelector('.form')
 
 function openform(form) {
-    form.classList.add(".form")
-    form.style.display = 'flex'
+  form.classList.add(".form")
+  form.style.display = 'flex'
+}
+// Em seguida vem o handler do submit
+// ainda não vai enviar para lugar nenhum
+
+// Observe que o nome da função começa com um verbo
+// e descreve exatamente o que a função faz
+function handleProfileFormSubmit(evt) {
+    // Esta linha impede o navegador
+    // de enviar o formulário da forma padrão.
+    evt.preventDefault();
+    // Fazendo isso, podemos definir nossa própria forma de enviar o formulário.
+    // Explicaremos em mais detalhes posteriormente.
+
+    // Vamos encontrar os campos de formulário do DOM
+    const nameInput = document.querySelector('.form__name-Input')
+    const jobInput = document.querySelector('.form__job-Input')
+
+    // Pegue os valores de cada campo do valor da propriedade correspondente
+    // Selecione os elementos aos quais os valores dos campos serão inseridos
+    const nameprofile = document.querySelector('.profile__name')
+    const jobprofile = document.querySelector('.profile__prof')
+    // Insira novos valores usando a
+    // propriedade textContent
+    nameprofile.textContent = nameInput.value
+    jobprofile.textContent = jobInput.value
 }
 
-function closeform(form) {
-    form.classList.remove('.form')
-    form.style.display = 'none'
+function closeform (form){
+  form.classList.remove('.form')
+  form.style.display = 'none'
 
 }
-function form__submit(evt){
-  evt.preventDefault();
-
-  const nameInput = document.querySelector('.form__name-Input')
-  const jobInput = document.querySelector('.form__job-Input')
-  
-
-}
-
 profbtn.addEventListener('click', ()=>{
 
-    openform(formEdit)
+  openform(formEdit)
 
 })
 
 formclose.addEventListener('click', ()=>{
-    closeform(formEdit)
+  closeform(formEdit)
 })
 
-formElement.addEventListener('submit', ()=>{
-
-  form__submit(formEdit)
-
-
-});
+// Conecte o handler ao formulário:
+// ele vai observar o evento de submit
+formElement.addEventListener('submit', handleProfileFormSubmit)
