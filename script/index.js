702f4e45db4eeff3525dfc7626dd74143e39d35e
erickwebdesigner <erickbarber10@gmail.com>
2024-02-10 08:45:29 -0300

    refactor: test js submit

--- script/index.js
+++ script/index.js
@@ -1,23 +1,26 @@
-const profbtn = document.querySelector('.profilebtn')
+const profbtn = document.querySelector('.profile__btn')
 const formEdit = document.querySelector('.form')
 const formclose = document.querySelector('.form__close')
 
-const nameprofile = document.querySelector('.profilename')
-const jobprofile = document.querySelector('.profileprof')
-const formname = document.querySelector('.form__name-Input')
-const formjob = document.querySelector('.form__job-Input')
+const formElement = document.querySelector('.form__submit')
 
 
 function openform(form) {
     form.classList.add(".form")
     form.style.display = 'flex'
-    formname.value = nameprofile.innerHTML
-    formjob.value = jobprofile.innerHTML
 }
 
 function closeform(form) {
     form.classList.remove('.form')
     form.style.display = 'none'
+
+}
+function form__submit(evt){
+  evt.preventDefault();
+
+  const nameInput = document.querySelector('.form__name-Input')
+  const jobInput = document.querySelector('.form__job-Input')
+  
 
 }
 
@@ -31,3 +34,9 @@ formclose.addEventListener('click', ()=>{
     closeform(formEdit)
 })
 
+formElement.addEventListener('submit', ()=>{
+
+  form__submit(formEdit)
+
+
+});
